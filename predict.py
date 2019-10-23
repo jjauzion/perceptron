_compute_cost corrected for softmax, gradient check is now ok

predict.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import numpy as np
 
 from src import processing
-import train
+from src import wrapper_fct
 
 
 parser = argparse.ArgumentParser()
@@ -12,7 +12,7 @@ parser.add_argument("-o", "--output", type=str, default="predicition.csv", help=
 parser.add_argument("model", type=str, help="model file to be used for prediction, pickle format")
 parser.add_argument("df_tool", type=str, help="pickle file with df scale and label")
 args = parser.parse_args()
-df_test = train.create_dataframe(args.df_file, header=None, scale=Path(args.df_tool))
+df_test = wrapper_fct.create_dataframe(args.df_file, header=None, scale=Path(args.df_tool))
 df_test.scale(exclude_col=1)
 X = np.delete(df_test.data, 1, axis=1)
 y = df_test.data[:, 1]

src/processing/NeuralNetwork.py
@@ -81,21 +81,21 @@ class NeuralNetwork(Model.Classification):
         """
         Perform a forward propagation but using custom weight instead of weight stored in self.
         This function is used only for gradient checking.
-        :param X:
-        :param weight:
-        :param layer:
-        :return:
+        :param X: input matrix
+        :param weight: weight matrix to be used instead of the weight stored in self
+        :param layer: layer where the provided weight matrix shall be used
+        :return: H, the computed hypothesis matrix
         """
         H = np.zeros((X.shape[0], self.topology[-1]))
         for m in range(X.shape[0]):
             h = np.copy(X[m])
-            for l in range(1, len(self.topology)):
+            for current_layer in range(1, len(self.topology)):
                 a = np.insert(h, 0, 1)
-                if l - 1 == layer:
+                if current_layer - 1 == layer:
                     z = np.matmul(weight, a)
                 else:
-                    z = np.matmul(self.weight[l - 1], a)
-                if layer == len(self.topology) - 1:
+                    z = np.matmul(self.weight[current_layer - 1], a)
+                if current_layer == len(self.topology) - 1:
                     h = self.activation_function(z)
                 else:
                     h = NeuralNetwork.sigmoid(z)
@@ -145,7 +145,10 @@ class NeuralNetwork(Model.Classification):
         :param H: m by nb_output_unit matrix, with m nb of sample. Matrix of the computed hypothesis Y with the current weight
         :return:
         """
-        cost = -1 / Y.shape[0] * (np.sum(np.row_stack(Y) * np.log(H) + (1 - np.row_stack(Y)) * np.log(1 - H)))
+        if self.activation_function != NeuralNetwork.softmax:
+            cost = -1 / Y.shape[0] * (np.sum(np.row_stack(Y) * np.log(H) + (1 - np.row_stack(Y)) * np.log(1 - H)))
+        else:
+            cost = -1 / Y.shape[0] * (np.sum(np.row_stack(Y) * np.log(H)))
         regul = 0
         for l in range(len(self.topology) - 1):
             regul += np.sum(self.weight[l] ** 2)

train.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--iteration", type=check_arg.is_positive_int, default=500, help="Number of iteration to run")
     parser.add_argument("-s", "--step", type=check_arg.is_positive_int, default=100, help="Step betweeen two print report")
     parser.add_argument("-m", "--model", type=str, help="load an existing model to pursue training")
+    parser.add_argument("-n", "--name", type=str, default="model", help="name of the model. Will be used to save the model to a pkl file")
     parser.add_argument("-a", "--act_fct", type=str, choices=["sigmoid", "softmax"], help="Activation function to be used in the output layer. Sigmoid by default")
     parser.add_argument("-v", "--verbose", type=int, choices=[0, 1, 2], default=0, help="Level of verbosity")
     args = parser.parse_args()
@@ -27,6 +28,7 @@ if __name__ == "__main__":
     print("Nb de 1 : ", np.count_nonzero(df_train.data[:, 1] == 1))
     print("Nb de 0 : ", np.count_nonzero(df_train.data[:, 1] == 0))
     df_test = wrapper_fct.create_dataframe(args.test_file, not args.no_header, scale=Path("model/data_train_scale.pkl"))
+    df_test.scale(exclude_col=1)
 
     if args.model is None:
         model = processing.NeuralNetwork(topology=args.topology, regularization_rate=0,
@@ -47,5 +49,5 @@ if __name__ == "__main__":
                 model.nb_iteration_ran, model.cost_history[-1], test_loss, model.f1score[0] * 100, test_f1score * 100))
     if args.verbose > 1:
         model.plot_training()
-    model.save_model(Path("model/m1.pkl"))
+    model.save_model(Path("model/{}.pkl".format(args.name)))
     wrapper_fct.check_test(df=df_test, model=model)